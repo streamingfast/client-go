f6e50c5f63baafc93e8cffd04fe17986718c9724
Matthieu Vachon <matthieu.vachon@dfuse.io>
2021-04-29 15:17:55 -0400

    Fixed problematic logging registration

--- logging.go
+++ logging.go
@@ -1,6 +1,13 @@
 package dfuse
 
-import "github.com/dfuse-io/logging"
+import (
+	"github.com/dfuse-io/logging"
+	"go.uber.org/zap"
+)
 
 var traceEnabled = logging.IsTraceEnabled("dfuse-client", "github.com/dfuse-io/client-go")
-var zlog = logging.NewSimpleLogger("dfuse-client", "github.com/dfuse-io/client-go")
+var zlog = zap.NewNop()
+
+func init() {
+	logging.Register("github.com/dfuse-io/client-go", &zlog)
+}
